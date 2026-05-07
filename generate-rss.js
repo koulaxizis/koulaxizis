dcd53a939bd16906434679333e48fc71086304eb
Christos Koulaxizis <ckoulaxizis@gmail.com>
2026-05-07 13:34:55 +0300

    Update generate-rss.js

--- generate-rss.js
+++ generate-rss.js
@@ -15,19 +15,31 @@ try {
   const siteTitle = 'Ενημερώσεις Χρήστου Κουλαξίζη';
   const siteDesc = 'Τελευταίες ενημερώσεις από τον Χρήστο Κουλαξίζη';
 
-  // Escape function για XML
+  // Escape function για XML (πιο αυστηρή)
   const escapeXml = (str) => {
     if (typeof str !== 'string') return '';
-    return str.replace(/[&<>"']/g, char => ({
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&apos;'
-    })[char]);
+    return str
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
   };
 
-  let xml = `<?xml version="1.0" encoding="UTF-8" ?>
+  // RFC 822 Date Format για pubDate
+  const toRFC822Date = (dateStr) => {
+    const date = new Date(dateStr);
+    if (isNaN(date.getTime())) {
+      return new Date().toUTCString();
+    }
+    return date.toUTCString();
+  };
+
+  // Ταξινόμηση (πιο πρόσφατα πρώτα)
+  const sortedUpdates = [...updates].sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  // Δημιουργία XML (χωρίς κενά πριν την declaration)
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>${escapeXml(siteTitle)}</title>
@@ -35,35 +47,32 @@ try {
     <description>${escapeXml(siteDesc)}</description>
     <language>el-gr</language>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    <atom:link href="${siteUrl}/feed.xml" rel="self" type="application/rss+xml" />
+    <atom:link href="${siteUrl}/feed.xml" rel="self" type="application/rss+xml"/>
 `;
 
-  // Ταξινόμηση (πιο πρόσφατα πρώτα)
-  const sortedUpdates = [...updates].sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  sortedUpdates.forEach(update => {
-    const date = new Date(update.date);
-    const pubDate = date.toUTCString();
+  sortedUpdates.forEach((update, index) => {
+    const pubDate = toRFC822Date(update.date);
     const content = escapeXml(update.content || '');
     const title = escapeXml(content.length > 60 ? content.substring(0, 60) + '...' : content);
+    
+    // GUID: Χρησιμοποιούμε timestamp + index για μοναδικότητα
+    const guid = `${siteUrl}/update-${Date.now()}-${index}`;
 
-    xml += `
-    <item>
+    xml += `    <item>
       <title>${title}</title>
       <description><![CDATA[${content}]]></description>
       <link>${siteUrl}/#updates</link>
-      <guid isPermaLink="false">${update.date}</guid>
+      <guid isPermaLink="false">${guid}</guid>
       <pubDate>${pubDate}</pubDate>
     </item>
 `;
   });
 
-  xml += `
-  </channel>
+  xml += `  </channel>
 </rss>`;
 
-  // 2. Γράψιμο στο αρχείο
-  fs.writeFileSync('feed.xml', xml);
+  // 2. Γράψιμο στο αρχείο (χωρίς BOM)
+  fs.writeFileSync('feed.xml', xml, 'utf8');
   console.log('RSS Feed generated successfully!');
   process.exit(0);
 
